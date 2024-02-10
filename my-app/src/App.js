845a9f0bbeec56a38f524266a0310d19bac6f3ba
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2024-02-10 01:49:42 +0000

    Brute Force Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -238,6 +238,39 @@ function Graph() {
     return weight;
   }
 
+  // From: https://www.30secondsofcode.org/js/s/array-permutations/
+  const permutations = arr => {
+    if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+    return arr.reduce(
+      (acc, item, i) =>
+        acc.concat(
+          permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+            item,
+            ...val,
+          ])
+        ),
+      []
+    );
+  };
+
+  const bruteForceTSP = () => {
+    var possible_tours = permutations([...Array(numNodes).keys()]);
+    var best_tour = [];
+    var best_weight = Number.MAX_VALUE;
+    for (let i = 0; i < possible_tours.length; i++) {
+      possible_tours[i].push(possible_tours[i][0]);
+      const tour = possible_tours[i];
+      var weight = tourWeight(tour);
+      if (weight < best_weight) {
+        best_tour = tour;
+        best_weight = weight;
+      }
+    }
+    console.log("Best Tour: " + best_tour + " Weight: " + best_weight);
+    return best_weight; // Return the weight of the best tour
+  };
+  
+
   const showWeight = (e, node1, node2) => {
     const weight = adjacencyMatrix[`${node1}-${node2}`];
     let displayText;
@@ -291,7 +324,6 @@ function Graph() {
             // Draw a line between the current node and the connected node
             const result = AdjMatrix[node1][node2] === 0; // Check if the value is not "NA"
             
-            const hover = false;
             return (
               // Check if node-node2 in the adjacency matrix has a value not "NA"
               result ? (
@@ -352,6 +384,7 @@ function Graph() {
         <button onClick={logWeights}>Log Weights</button>
         <button onClick={saveGraph}>Save Graph</button>
         <button onClick={loadGraph}>Load Graph</button>
+        <button onClick={bruteForceTSP}>TSP</button>
         <br/> <br/>
         <p1 id = "weight">Selected weight: NA </p1>
         {/* <button onClick={tspBruteForce}>Brute Force</button> */}
